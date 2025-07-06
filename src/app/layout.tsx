e48d1788fd4e8bf95bea4a8732eae6f3129c20c4
William-Liang808 <williamliang808@gmail.com>
2025-07-05 18:22:47 -1000

    Refactor file structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,10 @@
+import React from "react";
 import type {Metadata} from "next";
 import {Geist, Geist_Mono} from "next/font/google";
 import {Separator} from "@/components/ui/separator";
-import "./globals.css";
 import Navbar from "@/components/ui/Navbar";
 import {RecipeProvider} from "@/contexts/RecipeContext";
+import "./globals.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
